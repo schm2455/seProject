e04276b01a2eeaaa345375d31d49ac106e1d600c
TrevorButula <tdbutula@uwm.edu>
2021-04-25 19:47:44 -0500

    made columns for admin page

--- TAApp/views.py
+++ TAApp/views.py
@@ -7,26 +7,10 @@ class Login(View):
     def get(self, request):
         return render(request, "login.html", {})
 
+    def post(self, request):
+        return redirect("admin_home/")
+
 
 class Admin_home(View):
     def get(self, request):
         return render(request, "admin_home.html", {})
-
-    def post(self, request):
-        noSuchUser = False
-        badPassword = False
-        try:
-            m = MyUser.objects.get(name=request.POST['name'])
-            badPassword = (m.password != request.POST['password'])
-        except:
-            noSuchUser = True
-        if noSuchUser:
-            m = MyUser(name=request.POST['name'], password=request.POST['password'])
-            m.save()
-            request.session["name"] = m.name
-            return redirect("/things/")
-        elif badPassword:
-            return render(request, "login.html", {"message": "bad password"})
-        else:
-            request.session["name"] = m.name
-            return redirect("/things/")
