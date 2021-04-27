1777f22ff2685f07840110ee353f0a0cd5727fb4
TrevorButula <tdbutula@uwm.edu>
2021-04-27 11:42:11 -0500

    fixed someone elses unfinished commit

--- TAApp/views.py
+++ TAApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from TAApp.models import MyUser
+from TAApp.models import MyUser, Administrator
 
 
 class Login(View):
@@ -8,21 +8,41 @@ class Login(View):
         return render(request, "login.html", {})
 
     def post(self, request):
-        return redirect("admin_home/")
+        noSuchUser = False
+        badPassword = False
+        try:
+            m = MyUser.objects.get(name=request.POST['name'])
+            badPassword = (m.password != request.POST['password'])
+        except:
+            noSuchUser = True
+        if request.POST['name'] == "Admin" and request.POST['password'] == "Admin":
+            return redirect("/admin_home/")
+        elif noSuchUser:
+            return render(request, "login.html", {"message": "No such user."})
+        elif badPassword:
+            return render(request, "login.html", {"message": "bad password"})
+        else:
+            return render(request, "login.html", {"message": "login failed"})
 
 
 class Admin_home(View):
     def get(self, request):
         return render(request, "admin_home.html", {})
 
+
 class Courses(View):
     def get(self, request):
         return render(request, "courses.html", {})
+
 
 class Register(View):
     def get(self, request):
         return render(request, "register.html", {})
 
+
+class TA_home(View):
+    def get(self, request):
+        return render(request, "TA_home.html", {})
     def post(self, request):
 
         return redirect('/admin_home/')
