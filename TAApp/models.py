d3d33b892c681e84fbbffcde521920d0d1658464
TrevorButula <tdbutula@uwm.edu>
2021-04-26 19:08:48 -0500

    fixed background

--- TAApp/models.py
+++ TAApp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 
 
@@ -10,12 +11,14 @@ class MyUser(models.Model):
     def __str__(self):
         return self.name
 
+
 class Administrator(models.Model):
     name = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
 
     def __str__(self):
         return self.name
+
 
 class Instructor(models.Model):
     name = models.CharField(max_length=20)
@@ -24,6 +27,7 @@ class Instructor(models.Model):
     def __str__(self):
         return self.name
 
+
 class TA(models.Model):
     name = models.CharField(max_length=20)
     project_manager = models.ForeignKey(Instructor, on_delete=models.CASCADE)
@@ -31,12 +35,14 @@ class TA(models.Model):
     def __str__(self):
         return self.name
 
+
 class Course(models.Model):
     name = models.CharField(max_length=20)
     description = models.CharField(max_length=50)
     project_manager = models.ForeignKey(Administrator, on_delete=models.CASCADE, null=True, blank=True, default="")
     instructor = models.ManyToManyField(Instructor)
     instructorTA = models.ManyToManyField(TA)
+
 
 class Lab(models.Model):
     name = models.CharField(max_length=20)
