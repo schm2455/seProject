6837af54c9a298d91445ca81894b7e2e8f5b9c29
Arnav <amsaxena@uwm.edu>
2021-04-27 14:16:01 -0500

    trued the nulls in models

--- TAApp/models.py
+++ TAApp/models.py
@@ -4,43 +4,43 @@ from django.db import models
 
 
 class MyUser(models.Model):
-    name = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, null=True)
+    password = models.CharField(max_length=20, null=True)
 
     def __str__(self):
         return self.name
 
 class Administrator(models.Model):
-    name = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, null=True)
+    password = models.CharField(max_length=20, null=True)
 
     def __str__(self):
         return self.name
 
 class Instructor(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, null=True)
     project_manager = models.ForeignKey(Administrator, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
 class TA(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, null=True)
     project_manager = models.ForeignKey(Instructor, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
 class Course(models.Model):
-    name = models.CharField(max_length=20)
-    description = models.CharField(max_length=50)
+    name = models.CharField(max_length=20, null=True)
+    description = models.CharField(max_length=50, null=True)
     project_manager = models.ForeignKey(Administrator, on_delete=models.CASCADE, null=True, blank=True, default="")
     instructor = models.ForeignKey(Instructor, on_delete=models.CASCADE)
     instructorTA = models.ForeignKey(TA, on_delete=models.CASCADE)
 
 class Lab(models.Model):
-    name = models.CharField(max_length=20)
-    description = models.CharField(max_length=50)
+    name = models.CharField(max_length=20, null=True)
+    description = models.CharField(max_length=50, null=True)
     project_manager = models.ForeignKey(Instructor, on_delete=models.CASCADE, null=True, blank=True, default="")
     labTA = models.ForeignKey(TA, on_delete=models.CASCADE)
     labForCourse = models.ForeignKey(Course, on_delete=models.CASCADE)
