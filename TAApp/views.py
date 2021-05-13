77967c682d5c97cbdfe99a0fe628d8c53e697440
TrevorButula <tdbutula@uwm.edu>
2021-05-12 19:34:08 -0500

    can no longer navigate by url

--- TAApp/views.py
+++ TAApp/views.py
@@ -35,20 +35,40 @@ def direct(user_role):
         return None
 
 
+def valid(user_role, expected):
+    print(user_role + " " + expected)
+    if user_role == expected:
+        return True
+    else:
+        return False
+
+
 class Admin_home(View):
     def get(self, request):
-        TAList = list(map(str, TA.objects.all()))
-        InstructorList = list(map(str, Instructor.objects.all()))
-        CourseList = list(Course.objects.all())
-        return render(request, "admin_home.html", {"TAs": TAList, "instructors": InstructorList, "courses": CourseList})
+        loggedIn = request.session.get("name")
+        if MyUser.objects.filter(name=loggedIn).exists():
+            user = MyUser.objects.get(name=loggedIn)
+        if valid(user.role, "Admin"):
+            TAList = list(map(str, TA.objects.all()))
+            InstructorList = list(map(str, Instructor.objects.all()))
+            CourseList = list(Course.objects.all())
+            return render(request, "admin_home.html",
+                          {"TAs": TAList, "instructors": InstructorList, "courses": CourseList})
+        else:
+            return render(request, "login.html", {"message": "unauthorized access"})
 
 
 class Courses(View):
     def get(self, request):
-        instructors = list(map(str, Instructor.objects.all()))
-        tachoices = list(map(str, TA.objects.all()))
-
-        return render(request, "courses.html", {"instructors": instructors, "tachoices": tachoices})
+        loggedIn = request.session.get("name")
+        if MyUser.objects.filter(name=loggedIn).exists():
+            user = MyUser.objects.get(name=loggedIn)
+        if valid(user.role, "Admin") or valid(user.role, "Instructor"):
+            instructors = list(map(str, Instructor.objects.all()))
+            tachoices = list(map(str, TA.objects.all()))
+            return render(request, "courses.html", {"instructors": instructors, "tachoices": tachoices})
+        else:
+            return render(request, "login.html", {"message": "unauthorized access"})
 
     def post(self, request):
         coursename = request.POST['name']
@@ -62,7 +82,7 @@ class Courses(View):
         instructorchoice = Instructor.objects.get(name=instructorname)
         tachoice = TA.objects.get(name=taname)
 
-        user = MyUser.objects.get(name= request.session.get("name", False))
+        user = MyUser.objects.get(name=request.session.get("name", False))
 
         Course.objects.create(name=coursename, description=desc, instructor=instructorchoice, instructorTA=tachoice)
 
@@ -94,16 +114,31 @@ class Register(View):
             return render(request, "register.html", {"message": "Duplicate user"})
         return render(request, "login.html", {"message": "Success!"})
 
+
 class EditCourse(View):
     def get(self, request):
-        courses = list(Course.objects.all())
-        instructors = list(Instructor.objects.all())
-        tachoices = list(TA.objects.all())
-        return render(request, "editcourse.html", {"courses": courses, "instructors": instructors, "tachoices": tachoices})
+        loggedIn = request.session.get("name")
+        if MyUser.objects.filter(name=loggedIn).exists():
+            user = MyUser.objects.get(name=loggedIn)
+        if valid(user.role, "Admin"):
+            courses = list(Course.objects.all())
+            instructors = list(Instructor.objects.all())
+            tachoices = list(TA.objects.all())
+            return render(request, "editcourse.html",
+                          {"courses": courses, "instructors": instructors, "tachoices": tachoices})
+        else:
+            return render(request, "login.html", {"message": "unauthorized access"})
+
 
 class CreateTA(View):
     def get(self, request):
-        return render(request, "TAs.html", )
+        loggedIn = request.session.get("name")
+        if MyUser.objects.filter(name=loggedIn).exists():
+            user = MyUser.objects.get(name=loggedIn)
+        if valid(user.role, "Admin") or valid(user.role, "Instructor"):
+            return render(request, "TAs.html", )
+        else:
+            return render(request, "login.html", {"message": "unauthorized access"})
 
     def post(self, request):
         taname = request.POST.get('name')
@@ -118,7 +153,7 @@ class CreateTA(View):
         if not Instructor.objects.filter(name=instructorname).exists():
             return render(request, 'TAs.html', {"message": "Instructor does not exist"})
         TA.objects.create(name=taname, project_manager=Instructor.objects.get(name=instructorname))
-        user = MyUser.objects.get(name= request.session.get("name", False))
+        user = MyUser.objects.get(name=request.session.get("name", False))
 
         if direct(user.role) is not None:
             return redirect(direct(user.role).url)
@@ -128,24 +163,40 @@ class CreateTA(View):
 
 class TA_home(View):
     def get(self, request):
-        return render(request, "TA_home.html", {})
+        loggedIn = request.session.get("name")
+        if MyUser.objects.filter(name=loggedIn).exists():
+            user = MyUser.objects.get(name=loggedIn)
+        if valid(user.role, "TA"):
+            return render(request, "TA_home.html", {})
+        else:
+            return render(request, "login.html", {"message": "unauthorized access"})
 
 
 class Instructor_home(View):
     def get(self, request, ):
-        TAList = list(map(str, TA.objects.all()))
-        InstructorList = list(map(str, Instructor.objects.all()))
-        teacher = Instructor.objects.get(name=request.session.get("name", False))
-
-        CourseList = list(Course.objects.filter(instructor=teacher))
-        return render(request, "instructor_home.html",
-                      {"TAs": TAList, "instructors": InstructorList, "courses": CourseList, "teacher": teacher})
+        loggedIn = request.session.get("name")
+        if MyUser.objects.filter(name=loggedIn).exists():
+            user = MyUser.objects.get(name=loggedIn)
+        if valid(user.role, "Instructor"):
+            TAList = list(map(str, TA.objects.all()))
+            InstructorList = list(map(str, Instructor.objects.all()))
+            teacher = Instructor.objects.get(name=request.session.get("name", False))
+            CourseList = list(Course.objects.filter(instructor=teacher))
+            return render(request, "instructor_home.html",
+                          {"TAs": TAList, "instructors": InstructorList, "courses": CourseList, "teacher": teacher})
+        else:
+            return render(request, "login.html", {"message": "unauthorized access"})
 
 
 class CreateInstructor(View):
     def get(self, request):
-        loggedIn = request.session["name"]
-        return render(request, "instructors.html", {})
+        loggedIn = request.session.get("name")
+        if MyUser.objects.filter(name=loggedIn).exists():
+            user = MyUser.objects.get(name=loggedIn)
+        if valid(user.role, "Admin") or valid(user.role, "Instructor"):
+            return render(request, "instructors.html", {})
+        else:
+            return render(request, "login.html", {"message": "unauthorized access"})
 
     def post(self, request):
         loggedIn = request.session["name"]
